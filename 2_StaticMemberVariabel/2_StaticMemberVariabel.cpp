8d5aafa27c71c75dbea00fcf61569b877e61f890
sekarasriii <sekareeesh@gmail.com>
2024-05-13 11:08:49 +0700

    membuat fungsi main

--- 2_StaticMemberVariabel/2_StaticMemberVariabel.cpp
+++ 2_StaticMemberVariabel/2_StaticMemberVariabel.cpp
@@ -33,3 +33,16 @@ void mahasiswa::printAll()
 	cout << "Nama = " << nama << endl;
 	cout << endl;
 }
+
+int main()
+{
+	mahasiswa mhs1("Sekar");
+	mahasiswa mhs2("Kamilia");
+	mahasiswa mhs3("Zahwa");
+	mahasiswa mhs4("Wanda");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+}
