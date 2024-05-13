f4a2a63dbcb7bc759521832c68ed38e8ca7e87d8
sekarasriii <sekareeesh@gmail.com>
2024-05-13 10:30:58 +0700

    membuat variabel static yaitu nim

--- StaticMemberVariabel/StaticMemberVariabel.cpp
+++ StaticMemberVariabel/StaticMemberVariabel.cpp
@@ -7,20 +7,3 @@ class mahasiswa
 public:
 
 	static int nim;
-	int id;
-	string nama;
-
-	void setID();
-	void printAll();
-	mahasiswa(string pnama) : nama(pnama)
-	{
-		setID();
-	}
-};
-
-int mahasiswa::nim = 10;
-
-void mahasiswa::setID()
-{
-	id = ++nim;
-}
