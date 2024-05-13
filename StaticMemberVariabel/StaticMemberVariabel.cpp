8a5bf8cf48fb4e94314a0457a85918fb365a099e
sekarasriii <sekareeesh@gmail.com>
2024-05-13 10:28:56 +0700

    Add project files.

---
+++ StaticMemberVariabel/StaticMemberVariabel.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa
+{
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+	mahasiswa(string pnama) : nama(pnama)
+	{
+		setID();
+	}
+};
+
+int mahasiswa::nim = 10;
+
+void mahasiswa::setID()
+{
+	id = ++nim;
+}
